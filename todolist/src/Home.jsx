6639b7caf88d1c8400088b18a33a12fd17d430e3
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-03-23 12:56:05 +0530

    todo app incomplete

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect, useState} from "react"
+import React, { useEffect, useState } from "react"
 import Create from './Create'
 import axios from "axios";
 
@@ -6,8 +6,8 @@ function Home() {
     const [todos, setTodos] = useState([]);
     useEffect(() => {
         axios.get('http://localhost:3001/get')
-        .then(result=> setTodos(result.data))
-        .catch(err=> console.log(err))
+            .then(result => setTodos(result.data))
+            .catch(err => console.log(err))
     }, [])
     return (
         <div className="Home">
@@ -15,21 +15,27 @@ function Home() {
             <h2>Todo List</h2>
             <Create />
             {
-                todos.length === 0 
-                ?
-                <div><h2>no Record</h2></div>
-                :
-                todos.map(todo =>(
-                    <div>
-                       {todo.task}
-                    </div>
-                ))
+                todos.length === 0
+                    ?
+                    <div><h2>no Record</h2></div>
+                    :
+                    todos.map(todo => (
+                        <div className="task">
+                            <div className='checkbox'>
+                                <BsCircleFill className='icon' />
+                                <p>{todo.task}</p>
+                            </div>
+                            <div>
+                                <span><BsfillTrashFill className='icon' /></span>
+                            </div>
+                            </div>
+                            ))
                
                 
                 
-            } 
-        </div>
-    )
+            }
+                        </div>
+                    )
 }
 
-export default Home
+            export default Home
