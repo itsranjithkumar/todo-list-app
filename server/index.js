44c165131cdb142afdb850d6a59bbf6ddb7f72a7
itsranjithkumar <ranjithkumarms28@gmail.com>
2024-03-28 11:56:18 +0530

    todo list completed

--- server/index.js
+++ server/index.js
@@ -15,6 +15,13 @@ app.get('/get', (req, res)=> {
     .catch(err => res.json(err))
 })
 
+app.delete('/delete/:id', (req, res) => {
+    const {id} = req.params;
+    TodoModel.findByIdAndDelete({_id:id})
+    .then(result => res.json(result))
+    .catch(err => res.json(err))
+})
+
 app.put('/update/:id', (req, res) =>{
     console.log("inside put")
     const {id} = req.params;
